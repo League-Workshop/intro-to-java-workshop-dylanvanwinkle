294136c45a032b6ed5719714ac131a57413057f0
League Student <league@iMac-11.attlocal.net>
2019-07-09 14:31:17 -0700

    Dat #2
    Amazing Fun Doing Robots YAY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,29 +6,32 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot doom = new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		doom.setSpeed(100);
 		// 5. Set the pen width to 5
-
+doom.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
-			// 7. Set the pen color to random
-	
+for(int i = 0; i < 4; i++) {
+			// 7. Set the pen color to random	
+doom.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+drawSquare();	
 			// 8. Turn the robot 90 degrees to the right
-
+doom.turn(90);
 	}
-
+	}
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		//JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
-	}
+doom.penDown();
+doom.sparkle();
+		for(int i = 0; i < 4; i++) {
+doom.move(200);
+doom.turn(90);
+		}}
 
 	public static void main(String[] args) {
 		new FourSquare().go();
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,20 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+Robot a = new Robot();
 		// 5. Set your robot's pen down 
-		
+		a.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		a.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for(int i = 0; i < 75; i++) {
 			// 7. Change the pen color to random
-		
+		a.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+a.move(5*i);			
 			// 2. Turn the robot 360/7 degrees to the right
-		
+a.turn(360/7); 
 			// 8. Set the pen width to i
-			
-	}
+			a.setPenWidth(i);
+		}}
 }
