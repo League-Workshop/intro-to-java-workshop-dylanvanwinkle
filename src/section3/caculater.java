a763f4e971af686661856a76927b559bdbc39e51
Dylan <dylanvanwinkle@gmail.com>
2019-07-21 08:39:05 -0700

    w

---
+++ src/section3/caculater.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class caculater {
+
+	public static void main(String[] args) {
+
+int number1 = 0;
+int number2 = 0;
+int answer = 0;
+
+
+JOptionPane.showMessageDialog(null, "this is a adittion caclulater you will type in two numbers and get a answer ");
+String input = JOptionPane.showInputDialog(null, "type in your first number");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
