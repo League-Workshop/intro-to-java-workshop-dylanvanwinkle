9c80470d5f3c0043286893fff6e4fa84e644b9b5
Dylan <dylanvanwinkle@gmail.com>
2019-07-22 08:37:19 -0700

    dssfggeykhklgwjf

---
+++ src/section3/youRock.java
@@ -0,0 +1,17 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class youRock {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "you rock");
+
+        for (; true;) {
+            JOptionPane.showMessageDialog(null, "and you rock");
+
+        }
+
+    }
+}
