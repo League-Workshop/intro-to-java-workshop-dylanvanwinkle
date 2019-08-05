c94ab20afbc47c8a1f233a4ac888204417f79fa4
Dylan <dylanvanwinkle@gmail.com>
2019-08-05 08:42:31 -0700

    objects

--- src/section4/YouLost.java
+++ src/section4/YouLost.java
@@ -4,14 +4,34 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 
+final class AttackDef {
+    String attackName = "No Attack";
+    int attackDamage = 10;
+    int defense = 5;
+}
+
 public class YouLost {
 
+    public static void main(String[] args) {
 
+        
+        String Dylan = "My name is Dylan";
+        
+        AttackDef shout = new AttackDef();
+        shout.attackName = "Shout";
+        shout.attackDamage = 50;
 
-public static void main(String[] args) {
-    JOptionPane.showMessageDialog(null, "you lost");
-    for(;true;) {
-    JOptionPane.showMessageDialog(null, "and you lost");    
+        AttackDef yell = new AttackDef();
+        shout.attackName = "Yell";
+        shout.attackDamage = 15;
+        shout.defense = 20;
+        
+        String msg = "Attack '" + shout.attackName + "' does " + shout.attackDamage + " damage";
+        JOptionPane.showMessageDialog(null, msg);
+
+        JOptionPane.showMessageDialog(null, "you lost");
+        for (; true;) {
+            JOptionPane.showMessageDialog(null, "and you lost");
+        }
     }
-}
 }
